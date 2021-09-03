de3725c6001069c9609e5fcee309c247b14af320
oopsy <whoops.yao@gmail.com>
2021-09-03 20:40:51 +0800

    refactor: use react effect hook to set component
    Set main app component asynchronously by useEffect hook.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import Markdown from './components/Markdown'
+import React, { useState, useEffect } from 'react'
 
 const pageSelect = async (page) => {
     const { default: r } = await import(`../notes/${page}`)
@@ -7,21 +6,23 @@ const pageSelect = async (page) => {
 }
 
 export default () => {
-    const star = window.location.pathname.split('/')[1]
-    if (star) {
-        const [html, setHtml] = useState('')
-        // sub page
-        ;(async () => {
-            try {
-                const { html: rawHtml } = await pageSelect(star)
-                setHtml(rawHtml)
-            } catch {
-                setHtml(`<h1>404</h1>`)
+    const loadingElement = <h1>Loading</h1>
+    const [element, setElement] = useState(loadingElement)
+    useEffect(() => {
+        (async () => {
+            const star = window.location.pathname.split('/')[1]
+            if (star) {
+                const { default: Markdown } = await import('./components/Markdown')
+                try {
+                    const { html } = await pageSelect(star)
+                    setElement(<Markdown html={html} />)
+                } catch {
+                    setElement(<h1>404</h1>)
+                }
+            } else {
+                setElement(<h1>Hello World</h1>)
             }
         })()
-        return <Markdown html={html} />
-    } else {
-        // landing page
-        return <h1>Hello World</h1>
-    }
+    })
+    return element
 }
